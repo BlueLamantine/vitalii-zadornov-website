4cea7e0bdb0d5b1ee3cfc56f5c26e4786e65ff30
Viktoriia <urban.03al03@gmail.com>
2021-08-02 23:16:40 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('it works!');
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
